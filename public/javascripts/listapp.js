2601411cf19d402b60219134c952cb708c990245
Fernando Picasso <ferkka@hotmail.com>
2015-10-17 16:34:27 +0200

    Added dynamic generation of list items.

--- public/javascripts/listapp.js
+++ public/javascripts/listapp.js
@@ -1,3 +1,16 @@
+
 $(document).ready(function () {
-	
+		
+	(function () {
+		$('main ul li').remove();
+		$('main ul').append(getListItem(1, "Milk"));
+		$('main ul').append(getListItem(2, "Onion"));
+		$('main ul').append(getListItem(3, "Juice"));
+		$('main ul').append(getListItem(4, "Bread"));
+	})();
+
+	function getListItem(index, text) {
+		return '<li>' + text + '<input class="checkbox" name="checkbox' + index + '" value="value' + index + '" type="checkbox"></li>';
+	}
+
 });
